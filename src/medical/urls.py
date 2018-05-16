f5972fcab356494d0db629756637f3963ebe854f
oluochelvis <oduorelvis247@gmail.com>
2018-05-16 20:03:24 +0300

    created the home page URL conf

--- src/medical/urls.py
+++ src/medical/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,patterns,include
 from django.contrib import admin
+
+from marketing.views import HomePage
+
+urlpatterns = patterns('',
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # Marketing pages
+    url(r'^$', HomePage.as_view(), name="home"),
 ]
